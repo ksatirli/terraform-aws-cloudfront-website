c1c2933fc6cf49bd29616d02b735f892a3c24353
Kerim Satirli <k.satirli@gmail.com>
2022-10-08 18:44:25 +0200

    linting

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -1,7 +1,7 @@
 # The AWS Provider is set to retrieve configuration from the executing environment
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 # The AWS Provider is set to retrieve configuration from the executing environment
